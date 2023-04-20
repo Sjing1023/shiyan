fc7633ea4a0ccca30d819237fc7dac2013c58024
Sjing1023 <2540712535@qq.com>
2023-04-20 20:43:39 +0800

    Update User.java
    邓志劲111

--- 实验1/User.java
+++ 实验1/User.java
@@ -10,17 +10,17 @@ public class User {
       //������8�� //ʹ��new �������Ĭ�ϵĹ��췽����������car2
       car1.setPower(128);   
       car2.setPower(76);
-      System.out.println("car1�Ĺ����ǣ�"+car1.getPower());
-      System.out.println("car2�Ĺ����ǣ�"+car2.getPower());
+      System.out.println("car1="+car1.getPower());
+      System.out.println("car2="+car2.getPower());
       car1.speedUp(80);
       car2.speedUp(80);
      // ������9��  //car1����speedUp�������Լ���speed��ֵ����80
       //������10�� //car2����speedUp�������Լ���speed��ֵ����80
-      System.out.println("car1Ŀǰ���ٶȣ�"+car1.getSpeed());
-      System.out.println("car2Ŀǰ���ٶȣ�"+car2.getSpeed());
+      System.out.println("car1="+car1.getSpeed());
+      System.out.println("car2="+car2.getSpeed());
       car1.speedDown(10);
       car2.speedDown(20);
-      System.out.println("car1Ŀǰ���ٶȣ�"+car1.getSpeed());
-      System.out.println("car2Ŀǰ���ٶȣ�"+car2.getSpeed());
+      System.out.println("car1="+car1.getSpeed());
+      System.out.println("car2="+car2.getSpeed());
    }
 }
