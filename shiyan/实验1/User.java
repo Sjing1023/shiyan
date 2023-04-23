ecb3b4b3770e67101c4e8dd8055b195beaded135
Sjing1023 <128580075+Sjing1023@users.noreply.github.com>
2023-04-23 20:07:35 +0800

    Add files via upload

---
+++ shiyan/实验1/User.java
@@ -0,0 +1,26 @@
+public class User {
+   public static void main(String args[]) {
+      Vehicle car1;
+      Vehicle car2;
+      car1= new Vehicle();
+      car2 =new Vehicle();
+      car1.setPower(128);
+      car2.setPower(76);
+      //new Ĭ ϵĹ  췽          car1
+      //      8   //ʹ  new        Ĭ ϵĹ  췽          car2
+      car1.setPower(128);   
+      car2.setPower(76);
+      System.out.println("car1="+car1.getPower());
+      System.out.println("car2="+car2.getPower());
+      car1.speedUp(80);
+      car2.speedUp(80);
+     //       9    //car1    speedUp       Լ   speed  ֵ    80
+      //      10   //car2    speedUp       Լ   speed  ֵ    80
+      System.out.println("car1="+car1.getSpeed());
+      System.out.println("car2="+car2.getSpeed());
+      car1.speedDown(10);
+      car2.speedDown(20);
+      System.out.println("car1="+car1.getSpeed());
+      System.out.println("car2="+car2.getSpeed());
+   }
+}
